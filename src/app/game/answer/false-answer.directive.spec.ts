c220b89c3f132f429a91ef2249b76b6c9270fb50
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-03-19 10:02:12 +0200

    fix: fixed files locations

--- src/app/game/answer/false-answer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FalseAnswerDirective } from './false-answer.directive';
-
-describe('FalseAnswerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FalseAnswerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
