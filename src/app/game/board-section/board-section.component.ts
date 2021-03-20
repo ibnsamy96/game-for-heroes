761a3d39440cd4165d46cb5072c39c54cbf52917
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-03-20 19:59:35 +0200

    add: created a grid in the chess board

--- src/app/game/board-section/board-section.component.ts
+++ src/app/game/board-section/board-section.component.ts
@@ -10,17 +10,54 @@ export class BoardSectionComponent implements OnInit {
 
   constructor(private gameLogicService: GameLogicService) { }
 
+  squaresRows!: ((boolean | number)[])[];
   squaresNumber!: number;
   squares!: (boolean | number)[];
 
 
   ngOnInit(): void {
 
+
+
+    let elementsNumberPerArray: number = 0;
+
     this.squaresNumber = this.gameLogicService.getSquaresNumber()
+
+
     this.squares = Array(this.squaresNumber).fill(false)
     const randomQuestionSquareLocation = Math.floor(Math.random() * this.squaresNumber);
     this.squares[randomQuestionSquareLocation] = 1
-    console.log(this.squares);
+
+    if ([2, 10].includes(this.squaresNumber)) {
+      elementsNumberPerArray = 2
+      this.spliceArray(2, this.squares)
+    } else if ([6, 9, 12, 15].includes(this.squaresNumber)) {
+      elementsNumberPerArray = 3
+    }
+
+    this.squaresRows = this.spliceArray(elementsNumberPerArray, this.squares)
+
+  }
+
+
+  spliceArray(elementsNumberPerArray: number, oldArray: (boolean | number)[]) {
+
+    const newArray = []
+
+    console.log(oldArray);
+
+
+    while (oldArray.length > 0) {
+      const tempArr = oldArray.filter((value, index) => {
+        return index < elementsNumberPerArray
+      })
+      oldArray.splice(0, elementsNumberPerArray)
+      newArray.push(tempArr)
+    }
+
+    console.log(newArray);
+    return newArray
+
 
   }
 
