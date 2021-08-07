258d8df06fedfdfe5e3ad39f902c064e4f4c2048
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-08-07 11:27:56 +0200

    added declerative text to the dragger

--- src/app/game/answers-section/answers-section.component.ts
+++ src/app/game/answers-section/answers-section.component.ts
@@ -1,93 +1,144 @@
 import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
-import { GameLogicService } from "../../shared/game-logic.service";
+import { GameLogicService } from '../../shared/game-logic.service';
 
 @Component({
   selector: 'app-answers-section',
   templateUrl: './answers-section.component.html',
-  styleUrls: ['./answers-section.component.css']
+  styleUrls: ['./answers-section.component.css'],
 })
 export class AnswersSectionComponent implements OnInit {
+  answersCentersLocations: {
+    xCord: number;
+    yCord: number;
+    diameter: number;
+    element: HTMLElement;
+  }[] = [];
 
-  answersCentersLocations: { xCord: number, yCord: number, diameter: number, element: HTMLElement }[] = [];
+  constructor(
+    private gameLogicService: GameLogicService,
+    private router: Router,
+    private render: Renderer2
+  ) {}
 
-  constructor(private gameLogicService: GameLogicService, private router: Router, private render: Renderer2) { }
-
-  @Input() answers!: { text: string, state: boolean }[];
+  @Input() answers!: { text: string; state: boolean }[];
 
   ngOnInit(): void {
     console.log(this.answers);
   }
 
-  addAnswer(centerLocation: { xCord: number, yCord: number, diameter: number, element: HTMLElement }) {
-    this.answersCentersLocations.push(centerLocation)
+  addAnswer(centerLocation: {
+    xCord: number;
+    yCord: number;
+    diameter: number;
+    element: HTMLElement;
+  }) {
+    this.answersCentersLocations.push(centerLocation);
   }
 
-  checkLastChoiceLocation(centerLocation: { xCord: number, yCord: number, diameter: number, element: HTMLElement }): void {
+  checkLastChoiceLocation(centerLocation: {
+    xCord: number;
+    yCord: number;
+    diameter: number;
+    element: HTMLElement;
+  }): void {
+    const chosenAnswerElement = this.answersCentersLocations.find(
+      (answerCenterLocation) => {
+        const choiceXCenter = centerLocation.xCord;
+        const choiceYCenter = centerLocation.yCord;
 
-    const chosenAnswerElement = this.answersCentersLocations.find((answerCenterLocation) => {
-      const choiceXCenter = centerLocation.xCord
-      const choiceYCenter = centerLocation.yCord
+        const answerXStart =
+          answerCenterLocation.xCord - answerCenterLocation.diameter / 2;
+        const answerYStart =
+          answerCenterLocation.yCord - answerCenterLocation.diameter / 2;
 
-      const answerXStart = answerCenterLocation.xCord - (answerCenterLocation.diameter / 2)
-      const answerYStart = answerCenterLocation.yCord - (answerCenterLocation.diameter / 2)
+        const answerXEnd =
+          answerCenterLocation.xCord + answerCenterLocation.diameter / 2;
+        const answerYEnd =
+          answerCenterLocation.yCord + answerCenterLocation.diameter / 2;
 
-      const answerXEnd = answerCenterLocation.xCord + (answerCenterLocation.diameter / 2)
-      const answerYEnd = answerCenterLocation.yCord + (answerCenterLocation.diameter / 2)
-
-      return (choiceXCenter > answerXStart && choiceXCenter < answerXEnd) && (choiceYCenter > answerYStart && choiceYCenter < answerYEnd)
-    })
+        return (
+          choiceXCenter > answerXStart &&
+          choiceXCenter < answerXEnd &&
+          choiceYCenter > answerYStart &&
+          choiceYCenter < answerYEnd
+        );
+      }
+    );
 
     if (chosenAnswerElement) {
-      const chosenAnswerText = chosenAnswerElement.element.innerText
-      this.gameLogicService.submitAnswer(chosenAnswerText)
-      this.router.navigate(['result'])
+      const chosenAnswerText = chosenAnswerElement.element.innerText;
+      this.gameLogicService.submitAnswer(chosenAnswerText);
+      this.router.navigate(['result']);
     }
   }
 
-
   lastChosenAnswerCircle: Element | null | undefined; // variable holds the last chosen answer
-  isOutOfAnswers: boolean = true // boolean indicates that the choice is out of all of the answers
+  isOutOfAnswers: boolean = true; // boolean indicates that the choice is out of all of the answers
 
-  checkChoiceLocation(centerLocation: { xCord: number, yCord: number, diameter: number, element: HTMLElement }): void {
+  checkChoiceLocation(centerLocation: {
+    xCord: number;
+    yCord: number;
+    diameter: number;
+    element: HTMLElement;
+  }): void {
+    const chosenAnswerElement = this.answersCentersLocations.find(
+      (answerCenterLocation) => {
+        const choiceXCenter = centerLocation.xCord;
+        const choiceYCenter = centerLocation.yCord;
 
-    const chosenAnswerElement = this.answersCentersLocations.find((answerCenterLocation) => {
+        const answerXStart =
+          answerCenterLocation.xCord - answerCenterLocation.diameter / 2;
+        const answerYStart =
+          answerCenterLocation.yCord - answerCenterLocation.diameter / 2;
 
-      const choiceXCenter = centerLocation.xCord
-      const choiceYCenter = centerLocation.yCord
+        const answerXEnd =
+          answerCenterLocation.xCord + answerCenterLocation.diameter / 2;
+        const answerYEnd =
+          answerCenterLocation.yCord + answerCenterLocation.diameter / 2;
 
-      const answerXStart = answerCenterLocation.xCord - (answerCenterLocation.diameter / 2)
-      const answerYStart = answerCenterLocation.yCord - (answerCenterLocation.diameter / 2)
-
-      const answerXEnd = answerCenterLocation.xCord + (answerCenterLocation.diameter / 2)
-      const answerYEnd = answerCenterLocation.yCord + (answerCenterLocation.diameter / 2)
-
-      return (choiceXCenter > answerXStart && choiceXCenter < answerXEnd) && (choiceYCenter > answerYStart && choiceYCenter < answerYEnd)
-
-    })
+        return (
+          choiceXCenter > answerXStart &&
+          choiceXCenter < answerXEnd &&
+          choiceYCenter > answerYStart &&
+          choiceYCenter < answerYEnd
+        );
+      }
+    );
 
     if (this.isOutOfAnswers && this.lastChosenAnswerCircle) {
-      this.render.setStyle(this.lastChosenAnswerCircle, 'background-color', 'transparent')
-      this.render.setStyle(this.lastChosenAnswerCircle, 'border-color', '#79E8D8')
-      this.render.setStyle(this.lastChosenAnswerCircle, 'color', '#79E8D8')
-      this.lastChosenAnswerCircle = undefined
+      this.render.setStyle(
+        this.lastChosenAnswerCircle,
+        'background-color',
+        'transparent'
+      );
+      this.render.setStyle(
+        this.lastChosenAnswerCircle,
+        'border-color',
+        '#79E8D8'
+      );
+      this.render.setStyle(this.lastChosenAnswerCircle, 'color', '#79E8D8');
+      this.lastChosenAnswerCircle = undefined;
     }
 
     if (chosenAnswerElement?.element.firstElementChild) {
-      this.isOutOfAnswers = false
+      this.isOutOfAnswers = false;
 
-      this.lastChosenAnswerCircle = chosenAnswerElement?.element.firstElementChild
-      this.render.setStyle(this.lastChosenAnswerCircle, 'background-color', '#B53A57')
-      this.render.setStyle(this.lastChosenAnswerCircle, 'border-color', '#B53A57')
-      this.render.setStyle(this.lastChosenAnswerCircle, 'color', 'white')
-
+      this.lastChosenAnswerCircle =
+        chosenAnswerElement?.element.firstElementChild;
+      this.render.setStyle(
+        this.lastChosenAnswerCircle,
+        'background-color',
+        '#B53A57'
+      );
+      this.render.setStyle(
+        this.lastChosenAnswerCircle,
+        'border-color',
+        '#B53A57'
+      );
+      this.render.setStyle(this.lastChosenAnswerCircle, 'color', 'white');
     } else {
-      this.isOutOfAnswers = true
+      this.isOutOfAnswers = true;
     }
-
-
-
   }
-
-
 }
--- src/app/game/answers-section/choice/choice.component.ts
+++ src/app/game/answers-section/choice/choice.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-choice',
   templateUrl: './choice.component.html',
-  styleUrls: ['./choice.component.css']
+  styleUrls: ['./choice.component.css'],
 })
 export class ChoiceComponent implements OnInit {
+  @Input() innerText: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
